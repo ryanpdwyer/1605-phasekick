3d251c23166bc8cfdc5b5c818a368fd18c7d8eca
Ryan Dwyer <ryanpdwyer@gmail.com>
2016-11-09 14:42:43 -0500

    Updated the code for modeling data directly in the time domain

--- notebooks/dc-ac-separation/timedomainfit.py
+++ notebooks/dc-ac-separation/timedomainfit.py
@@ -178,157 +178,157 @@ def fit(f, xdata, ydata, p0=None, bounds=(-np.inf, np.inf), tfit=None, name=''):
 
 
 def make_optfunc(t0, tp, f_LP=200e3, f_HP=1e3, fs=1e6, Q=30000):
-#     """This huge function currently implements all of the various curve fitting options developed."""
+    """This huge function currently implements all of the various curve fitting options developed."""
 
-#     b, a = signal.butter(2, np.array([f_HP, f_LP]) / (fs/2), analog=False, btype='bandpass')
+    b, a = signal.butter(2, np.array([f_HP, f_LP]) / (fs/2), analog=False, btype='bandpass')
 
-#     def phit(t, f_i, df_light, f_f, tau):
-#         t = t - t0
-#         tp_ = tp - t0
-#         return 2*np.pi*np.where(t <= 0, f_i*t,
-#                       np.where(t < tp_, f_i*t+ pk.phase_step(t, tau, df_light),
-#                               f_i*tp_+ pk.phase_step(tp_, tau, df_light) + f_f*(t-tp_)))
+    def phit(t, f_i, df_light, f_f, tau):
+        t = t - t0
+        tp_ = tp - t0
+        return 2*np.pi*np.where(t <= 0, f_i*t,
+                      np.where(t < tp_, f_i*t+ pk.phase_step(t, tau, df_light),
+                              f_i*tp_+ pk.phase_step(tp_, tau, df_light) + f_f*(t-tp_)))
     
-#     def get_ft(f_i, df, f_f, tau):
-#         def ft(t):
-#             t = t - t0
-#             tp_ = tp - t0
-#             return np.where(t <= 0, f_i,
-#                       np.where(t < tp_, f_i - df_light *np.expm1(-t/tau), f_f)
-#                             )
-
-    
-#     def xDCt(t, phaset, ft, tau, xDC0, dx_light, A):
-#         t_ = t - t0
-#         tp_ = tp - t0
-        
-#         delta = (ft(tp) / ft(t0))**2
-#         r = 1 - delta
-        
-#         omega_tau = (2*np.pi*ft(t0)*tau)
-#         omega_tau2 = omega_tau**2
-#         omega_bar = (phaset(tp) - phaset(t0)) / (tp - t0)
-#         xeq = lambda t: np.where(t <= t0, xDC0, np.where(t < tp, xDC0-dx_light*np.expm1(-(t-t0)/tau),0))
-        
-#         xresp = lambda t: np.where(t <= t0, xDC0, np.where(t < tp, ))
-
-#         return (r*(
-#                 xDC0 + dx_light -
-#                 dx_light * omega_tau2 / (1+omega_tau2) * np.exp(-t_/tau)
-#                 ) + 
-#                 delta*xDC0*np.cos(omega_bar*t) -
-#                 dx_light * r /(1+omega_tau2) * (
-#                     np.cos(omega_bar * t) + omega_tau*np.sin(omega_bar * t)
-#                 ))
-
-#     def vDCt(t, f_i, df_light, f_f, tau, xDC0, dx_light):
-#         t = t - t0
-#         tp_ = tp - t0
-#         delta = (f_f / f_i)**2
-#         r = 1 - delta
-#         omega_tau = (2*np.pi*f_i*tau)
-#         omega_tau2 = omega_tau**2
-#         omega_bar = 2*np.pi * (
-#                         phit(tp, f_i, df_light, f_f, tau) -
-#                          phit(t0, f_i, df_light, f_f, tau)
-#                         ) / (tp - t0)
-#         return (r*dx_light*omega_tau/(1+omega_tau2)*np.exp(-t/tau) -
-#                 2*np.pi*delta*f_i* xDC0 * np.sin(omega_bar*t) -
-#                 dx_light * r * omega_bar /(1+omega_tau2) * (
-#                     np.sin(omega_bar * t) - omega_tau*np.cos(omega_bar * t)
-#                 ))
-    
-    
-#     def xDC_all(t, f_i, df_light, f_f, X0, Y0, tau, xDC0, dx_light):
-#         t_ = t - t0
-#         tp_ = tp - t0
-#         k_ringdown = 2*np.pi*f_i / Q
-        
-#         phi = phit(t, f_i, df_light, f_f, tau)
-#         return np.where(t_ < 0, xDC0,
-#                       np.where(t_ < tp_,
-#                         xDCt(t, f_i, df_light, f_f, tau, xDC0, dx_light),
-#                         (xDCt(tp, f_i, df_light, f_f, tau, xDC0, dx_light) * np.cos(2*np.pi*f_f*(t-tp)) + 
-#                         vDCt(tp, f_i, df_light, f_f, tau, xDC0, dx_light) * np.sin(2*np.pi*f_f*(t-tp)) / (2*np.pi*f_f)
-#                          ) * np.exp(-t*k_ringdown)
-#                               ))
-        
-#     def optfuncDC(t, f_i, df_light, f_f, X0, Y0, tau, Q0, Qp, xDC0, dx_light,):
-#         t_ = t - t0
-#         tp_ = tp - t0
-#         k_ringdown = 2*np.pi*f_i / Q
-        
-#         phase = pk_phase(f_i, df_light, f_f, tau, t0, tp)
-#         freq = pk_freq(f_i, df_light, f_f, tau, t0, tp)
-        
-#         k_ringdown = 2*np.pi*f_i / Q0
-#         k_light = 2*np.pi*f_i / Qp
-#         Ap = np.exp(-k_light * (tp-t0))
-#         A = getA(k_ringdown, k_light, k_ringdown, t0, tp)
-        
-#         xDC = np.where(t_ < 0, xDC0,
-#                       np.where(t_ < tp_,
-#                         (xDCt(t, phase, freq, tau, xDC0, dx_light, A) - xDC0)*np.exp(-k_light*t)+xDC0,
-#                         (xDCt(t, phase, freq, tau, xDC0, dx_light, A) * np.cos(2*np.pi*f_f*(t-tp)) + 
-#                          vDCt(tp, f_i, df_light, f_f, tau, xDC0, dx_light) * np.sin(2*np.pi*f_f*(t-tp)) / (2*np.pi*f_f)
-#                          ) * Ap * np.exp(-k_ringdown * (t-tp))
-#                               ))
-
-#         xout = osc_phase(t, phase, A, X0, Y0) + xDC
-#         return signal.lfilter(b, a, xout)[500:]
-    
-#     def optfuncDC2(t, f_i, df_light, f_f, X0, Y0, tau, Q0, Qp, xDC0, dx_light, tau2, ratio):
-#         t_ = t - t0
-#         tp_ = tp - t0
-#         k_ringdown = 2*np.pi*f_i / Q
-#         df1 = ratio * df_light
-#         df2 = (1-ratio) * df_light
-#         phi = phit(t, f_i, df1, f_f, tau) + phit(t, 0.0, df2, 0.0, tau2)
-        
-        
-#         k_ringdown = 2*np.pi*f_i / Q0
-#         k_light = 2*np.pi*f_i / Qp
-#         Ap = np.exp(-k_light * (tp-t0))
-        
-#         xDC = np.where(t_ < 0, xDC0,
-#                       np.where(t_ < tp_,
-#                         (xDCt(t, f_i, df_light, f_f, tau, xDC0, dx_light) - xDC0)*np.exp(-k_light*t)+xDC0,
-#                         (xDCt(tp, f_i, df_light, f_f, tau, xDC0, dx_light) * np.cos(2*np.pi*f_f*(t-tp)) + 
-#                          vDCt(tp, f_i, df_light, f_f, tau, xDC0, dx_light) * np.sin(2*np.pi*f_f*(t-tp)) / (2*np.pi*f_f)
-#                          ) * Ap * np.exp(-k_ringdown * (t-tp))
-#                               ))
-#         xout = ((X0 * np.cos(phi) + Y0 * np.sin(phi))*np.where(t < 0, np.exp(-t*k_ringdown),
-#                                                                 np.where(t < tp, np.exp(-k_light*t),
-#                                                                         Ap * np.exp(-k_ringdown * (t-tp))))
-#                 + xDC)
-#         return signal.lfilter(b, a, xout)[500:]
+    def get_ft(f_i, df, f_f, tau):
+        def ft(t):
+            t = t - t0
+            tp_ = tp - t0
+            return np.where(t <= 0, f_i,
+                      np.where(t < tp_, f_i - df_light *np.expm1(-t/tau), f_f)
+                            )
 
     
-#     def optfunc(t, f_i, df_light, f_f, X0, Y0, tau):
-#         t = t - t0
-#         k_ringdown = 2*np.pi*f_i / Q
-#         phi = phit(t, f_i, df_light, f_f, tau)
-#         return (X0 * np.cos(phi) + Y0 * np.sin(phi))*np.exp(-t*k_ringdown)
+    def xDCt(t, phaset, ft, tau, xDC0, dx_light, A):
+        t_ = t - t0
+        tp_ = tp - t0
+        
+        delta = (ft(tp) / ft(t0))**2
+        r = 1 - delta
+        
+        omega_tau = (2*np.pi*ft(t0)*tau)
+        omega_tau2 = omega_tau**2
+        omega_bar = (phaset(tp) - phaset(t0)) / (tp - t0)
+        xeq = lambda t: np.where(t <= t0, xDC0, np.where(t < tp, xDC0-dx_light*np.expm1(-(t-t0)/tau),0))
+        
+        xresp = lambda t: np.where(t <= t0, xDC0, np.where(t < tp, ))
+
+        return (r*(
+                xDC0 + dx_light -
+                dx_light * omega_tau2 / (1+omega_tau2) * np.exp(-t_/tau)
+                ) + 
+                delta*xDC0*np.cos(omega_bar*t) -
+                dx_light * r /(1+omega_tau2) * (
+                    np.cos(omega_bar * t) + omega_tau*np.sin(omega_bar * t)
+                ))
+
+    def vDCt(t, f_i, df_light, f_f, tau, xDC0, dx_light):
+        t = t - t0
+        tp_ = tp - t0
+        delta = (f_f / f_i)**2
+        r = 1 - delta
+        omega_tau = (2*np.pi*f_i*tau)
+        omega_tau2 = omega_tau**2
+        omega_bar = 2*np.pi * (
+                        phit(tp, f_i, df_light, f_f, tau) -
+                         phit(t0, f_i, df_light, f_f, tau)
+                        ) / (tp - t0)
+        return (r*dx_light*omega_tau/(1+omega_tau2)*np.exp(-t/tau) -
+                2*np.pi*delta*f_i* xDC0 * np.sin(omega_bar*t) -
+                dx_light * r * omega_bar /(1+omega_tau2) * (
+                    np.sin(omega_bar * t) - omega_tau*np.cos(omega_bar * t)
+                ))
     
-#     def optfuncDet(t, f_i, df_light, f_f, X0, Y0, tau, Q0, Qp):
-#         t = t - t0
-#         k_ringdown = 2*np.pi*f_i / Q0
-#         k_light = 2*np.pi*f_i / Qp
-#         phi = phit(t, f_i, df_light, f_f, tau)
-#         Ap = np.exp(-k_light * (tp-t0))
-#         xout = (X0 * np.cos(phi) + Y0 * np.sin(phi))*np.where(t < 0, np.exp(-t*k_ringdown),
-#                                                                 np.where(t < tp, np.exp(-k_light*t),
-#                                                                         Ap * np.exp(-k_ringdown * (t-tp))))
-#         return signal.lfilter(b, a, xout)[500:]
+    
+    def xDC_all(t, f_i, df_light, f_f, X0, Y0, tau, xDC0, dx_light):
+        t_ = t - t0
+        tp_ = tp - t0
+        k_ringdown = 2*np.pi*f_i / Q
+        
+        phi = phit(t, f_i, df_light, f_f, tau)
+        return np.where(t_ < 0, xDC0,
+                      np.where(t_ < tp_,
+                        xDCt(t, f_i, df_light, f_f, tau, xDC0, dx_light),
+                        (xDCt(tp, f_i, df_light, f_f, tau, xDC0, dx_light) * np.cos(2*np.pi*f_f*(t-tp)) + 
+                        vDCt(tp, f_i, df_light, f_f, tau, xDC0, dx_light) * np.sin(2*np.pi*f_f*(t-tp)) / (2*np.pi*f_f)
+                         ) * np.exp(-t*k_ringdown)
+                              ))
+        
+    def optfuncDC(t, f_i, df_light, f_f, X0, Y0, tau, Q0, Qp, xDC0, dx_light,):
+        t_ = t - t0
+        tp_ = tp - t0
+        k_ringdown = 2*np.pi*f_i / Q
+        
+        phase = pk_phase(f_i, df_light, f_f, tau, t0, tp)
+        freq = pk_freq(f_i, df_light, f_f, tau, t0, tp)
+        
+        k_ringdown = 2*np.pi*f_i / Q0
+        k_light = 2*np.pi*f_i / Qp
+        Ap = np.exp(-k_light * (tp-t0))
+        A = getA(k_ringdown, k_light, k_ringdown, t0, tp)
+        
+        xDC = np.where(t_ < 0, xDC0,
+                      np.where(t_ < tp_,
+                        (xDCt(t, phase, freq, tau, xDC0, dx_light, A) - xDC0)*np.exp(-k_light*t)+xDC0,
+                        (xDCt(t, phase, freq, tau, xDC0, dx_light, A) * np.cos(2*np.pi*f_f*(t-tp)) + 
+                         vDCt(tp, f_i, df_light, f_f, tau, xDC0, dx_light) * np.sin(2*np.pi*f_f*(t-tp)) / (2*np.pi*f_f)
+                         ) * Ap * np.exp(-k_ringdown * (t-tp))
+                              ))
 
-#     def optfuncQ(t, f_i, df_light, f_f, X0, Y0, tau, Q0, Qp):
-#         t = t - t0
-#         k_ringdown = 2*np.pi*f_i / Q0
-#         k_light = 2*np.pi*f_i / Qp
-#         phi = phit(t, f_i, df_light, f_f, tau)
-#         Ap = np.exp(-k_light * (tp-t0))
-#         return (X0 * np.cos(phi) + Y0 * np.sin(phi)) * np.where(t < 0, np.exp(-t*k_ringdown),
-#                                                                 np.where(t < tp, np.exp(-k_light*t),
-#                                                                         Ap * np.exp(-k_ringdown * (t-tp))))
+        xout = osc_phase(t, phase, A, X0, Y0) + xDC
+        return signal.lfilter(b, a, xout)[500:]
+    
+    def optfuncDC2(t, f_i, df_light, f_f, X0, Y0, tau, Q0, Qp, xDC0, dx_light, tau2, ratio):
+        t_ = t - t0
+        tp_ = tp - t0
+        k_ringdown = 2*np.pi*f_i / Q
+        df1 = ratio * df_light
+        df2 = (1-ratio) * df_light
+        phi = phit(t, f_i, df1, f_f, tau) + phit(t, 0.0, df2, 0.0, tau2)
+        
+        
+        k_ringdown = 2*np.pi*f_i / Q0
+        k_light = 2*np.pi*f_i / Qp
+        Ap = np.exp(-k_light * (tp-t0))
+        
+        xDC = np.where(t_ < 0, xDC0,
+                      np.where(t_ < tp_,
+                        (xDCt(t, f_i, df_light, f_f, tau, xDC0, dx_light) - xDC0)*np.exp(-k_light*t)+xDC0,
+                        (xDCt(tp, f_i, df_light, f_f, tau, xDC0, dx_light) * np.cos(2*np.pi*f_f*(t-tp)) + 
+                         vDCt(tp, f_i, df_light, f_f, tau, xDC0, dx_light) * np.sin(2*np.pi*f_f*(t-tp)) / (2*np.pi*f_f)
+                         ) * Ap * np.exp(-k_ringdown * (t-tp))
+                              ))
+        xout = ((X0 * np.cos(phi) + Y0 * np.sin(phi))*np.where(t < 0, np.exp(-t*k_ringdown),
+                                                                np.where(t < tp, np.exp(-k_light*t),
+                                                                        Ap * np.exp(-k_ringdown * (t-tp))))
+                + xDC)
+        return signal.lfilter(b, a, xout)[500:]
 
-#     return phit, optfunc, optfuncQ, optfuncDC, optfuncDet, xDCt, xDC_all
+    
+    def optfunc(t, f_i, df_light, f_f, X0, Y0, tau):
+        t = t - t0
+        k_ringdown = 2*np.pi*f_i / Q
+        phi = phit(t, f_i, df_light, f_f, tau)
+        return (X0 * np.cos(phi) + Y0 * np.sin(phi))*np.exp(-t*k_ringdown)
+    
+    def optfuncDet(t, f_i, df_light, f_f, X0, Y0, tau, Q0, Qp):
+        t = t - t0
+        k_ringdown = 2*np.pi*f_i / Q0
+        k_light = 2*np.pi*f_i / Qp
+        phi = phit(t, f_i, df_light, f_f, tau)
+        Ap = np.exp(-k_light * (tp-t0))
+        xout = (X0 * np.cos(phi) + Y0 * np.sin(phi))*np.where(t < 0, np.exp(-t*k_ringdown),
+                                                                np.where(t < tp, np.exp(-k_light*t),
+                                                                        Ap * np.exp(-k_ringdown * (t-tp))))
+        return signal.lfilter(b, a, xout)[500:]
+
+    def optfuncQ(t, f_i, df_light, f_f, X0, Y0, tau, Q0, Qp):
+        t = t - t0
+        k_ringdown = 2*np.pi*f_i / Q0
+        k_light = 2*np.pi*f_i / Qp
+        phi = phit(t, f_i, df_light, f_f, tau)
+        Ap = np.exp(-k_light * (tp-t0))
+        return (X0 * np.cos(phi) + Y0 * np.sin(phi)) * np.where(t < 0, np.exp(-t*k_ringdown),
+                                                                np.where(t < tp, np.exp(-k_light*t),
+                                                                        Ap * np.exp(-k_ringdown * (t-tp))))
+
+    return phit, optfunc, optfuncQ, optfuncDC, optfuncDet, xDCt, xDC_all
