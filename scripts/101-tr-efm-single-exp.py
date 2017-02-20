eb709ae90260f123aa5a22eed5344366a04f36de
Ryan Dwyer <ryanpdwyer@gmail.com>
2017-02-20 13:58:20 -0500

    Added single exponential fits

---
+++ scripts/101-tr-efm-single-exp.py
@@ -0,0 +1,111 @@
+from __future__ import division
+from os import path
+from tqdm import tqdm
+import numpy as np
+import phasekick
+import phasekickstan as p
+import time
+from collections import OrderedDict
+import lockin
+import h5py
+import datetime
+
+# Estimated run time 15 min, rMBP 2012
+
+print("tr-EFM Signal averaged curve fitting")
+print("====================================")
+print("\n")
+
+def arrays_to_stan_data(t, mu_df, sigma_df, N):
+    df_std = sigma_df / N**0.5
+    t = t * 1e3 # s to ms
+    m = t <= 0
+    y_neg = mu_df[m]
+    offset = np.mean(y_neg)
+    return {
+    'y_neg': y_neg - offset,
+    't': t[~m], # only positive times
+    'N_neg': t[m].size,
+    'y_neg_err': df_std[m],
+    'y': mu_df[~m] - offset,
+    'y_err': df_std[~m],
+    'offset': offset,
+    'N': t[~m].size
+    }
+
+outdir = '../results/tr-efm-pystan'
+
+grs = OrderedDict((
+    ('151217-200319-p1sun-df',
+    {
+ 'mu_df0': 0,
+ 'mu_df_inf': -13,
+ 'mu_tau': np.array([3.,15.]),
+ 'sigma_df0': 3,
+ 'sigma_df_inf': 5,
+ 'sigma_tau': np.array([5., 10.])}
+    ),
+    ('151217-205007-p3sun-df',
+    {
+ 'mu_df0': 0,
+ 'mu_df_inf': -15,
+ 'mu_tau': np.array([1., 5.]),
+ 'sigma_df0': 3,
+ 'sigma_df_inf': 5,
+ 'sigma_tau': np.array([4, 15.])}
+    ),
+    ('151217-211131-1sun-df',
+{
+ 'mu_df0': 0,
+ 'mu_df_inf': -15,
+ 'mu_tau': np.array([0.5,2]),
+ 'sigma_df0': 3,
+ 'sigma_df_inf': 5,
+ 'sigma_tau': np.array([1, 2])}
+    ),
+    ('151217-234238-20sun-df-384',
+{
+ 'mu_df0': 0,
+ 'mu_df_inf': -20,
+ 'mu_tau': np.array([0.1,1]),
+ 'sigma_df0': 2.5,
+ 'sigma_df_inf': 7,
+ 'sigma_tau': np.array([0.5, 2])}
+    ),
+    ('151218-003450-100sun-784',
+{
+ 'mu_df0': 0,
+ 'mu_df_inf': -35,
+ 'mu_tau': np.array([0.1,1]),
+ 'sigma_df0': 2.5,
+ 'sigma_df_inf': 8,
+ 'sigma_tau': np.array([0.5, 2])}
+    )
+)
+)
+
+file = '../results/tr-efm/tr-efm.h5'
+
+start = time.time()
+i = 0
+total = len(grs)
+with h5py.File(file, 'r') as fh:
+    for gr_name, priors in grs.items():
+        print(datetime.datetime.isoformat(datetime.datetime.now())[11:-7])
+        gr = fh[gr_name]
+        out_path = path.join(outdir, gr_name+'-dflive_single.h5')
+        data = arrays_to_stan_data(gr['t'].value, gr['mu_df'].value,
+                                   gr['sigma_df'].value, gr['N'].value)
+        # Use mean of double exponential prior
+        priors['mu_tau'] = np.mean(priors['mu_tau'])
+        priors['sigma_tau'] = np.mean(priors['mu_tau'])
+
+        pm = p.PhasekickModel('dflive', data, priors=priors)
+
+        pm.run(chains=4, iter=3000)
+        pm.save(out_path)
+        diff = time.time() - start
+        i += 1
+        print('{}/{} Elapsed: {:.1f} s, Remaining: {:.1f} s'.format(i, total, diff, diff/i*(total-i)
+            ))
+
---
+++ scripts/102-pk-efm-stan-single.py
@@ -0,0 +1,91 @@
+from __future__ import division
+import os
+from tqdm import tqdm
+import time
+import copy
+import numpy as np
+import phasekickstan as p
+
+files = {
+'../results/pk-efm/151217-201951-p1sun-phasekick.csv': 0.1,
+'../results/pk-efm/151217-205912-p3sun-phasekick.csv': 0.3,
+'../results/pk-efm/151217-214045-1sun-phasekick.csv': 1,
+'../results/pk-efm/151217-220252-1sun-phasekick-shorter.csv': 1,
+'../results/pk-efm/151217-233507-20sun-phasekick.csv': 20,
+'../results/pk-efm/151218-001254-20sun-phasekick-short.csv': 20,
+'../results/pk-efm/151218-002059-20sun-phasekick-short.csv': 20,
+'../results/pk-efm/151218-004818-100sun-phasekick.csv': 100,
+'../results/pk-efm/151218-011055-100sun-phasekick-768.csv': 100,
+'../results/pk-efm/151218-012858-20sun-phasekick-768.csv': 20,
+}
+
+def tau_prior(intensity):
+    return 0.18 + 0.6 / intensity
+
+def df_prior(intensity):
+    return -13 + -0.2 * intensity
+
+exp_priors = {
+    'mu_tau': 2.0,
+    'sigma_tau': 5.0,
+    'mu_df_inf': -20,
+    'sigma_df_inf': 15,
+}
+
+
+m_exp = 'exp_sq_no_control'
+
+outdir = '../results/pk-efm-pystan/'
+
+def main(files, chains=4, iterations=10):
+    now = time.time()
+    i = 0
+    itot = len(files)
+
+    for fname, intensity in files.items():
+        basename = os.path.splitext(fname)[0]
+        pk1 = p.PhasekickModel(m_exp, fname)
+
+        folder, filename = os.path.split(basename)
+
+        print(basename)
+        print("\n\n")
+
+        print(time.strftime("%H:%M:%S",time.localtime()))
+        print("\n\n")
+
+        new_priors = copy.copy(exp_priors)
+
+        new_priors['mu_tau'] = exp_priors['mu_tau'] * tau_prior(intensity)
+        new_priors['sigma_tau'] = exp_priors['sigma_tau']
+        new_priors['mu_df_inf'] = df_prior(intensity)
+
+
+        pk1.run(chains=chains, iter=iterations, priors=new_priors)
+        sample_fname = os.path.join(outdir, 'test11'+filename+'_'+'exp_sq_nc'+'.h5')
+        pk1.save(sample_fname)
+
+        i += 1
+        elapsed = time.time() - now
+
+        print("{}/{} complete in {:.2f} min. Estimated {:.1f} min remaining.".format(
+            i, itot, elapsed / 60., elapsed / 60. * (itot - i) / i))
+
+if __name__ == '__main__':
+    import sys
+    argc = len(sys.argv)
+    
+    iterations = 10
+    chains = 1
+
+    if argc > 3:
+        raise ValueError("Too many arguments")
+    if argc > 2:
+        iterations = int(sys.argv[2])
+    if argc > 1:
+        if ('-h' in str(sys.argv[1])):
+            print("Usage: python 002-pk-efm-stan.py [chains] [iterations]")
+            sys.exit()
+        chains = int(sys.argv[1])
+
+    main(files, chains, iterations)
